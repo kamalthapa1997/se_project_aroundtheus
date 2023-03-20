6eac52b2de82715f814d1ffd708436e4a39596cb
Kamal Thapa <handsomelotus1@cmail.com>
2023-03-20 12:48:12 -0400

    new update for my project

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-const initialcards = [
+const initialCards = [
   {
     name: "Yosemite Valley",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
@@ -24,4 +24,4 @@ const initialcards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
-console.log(initialcards);
+console.log(initialCards);
