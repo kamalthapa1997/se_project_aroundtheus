0be7088d5900b9e63b3879eae3aeec6eafde8798
Kamal Thapa <handsomelotus1@cmail.com>
2023-05-26 15:15:04 -0400

    feat: installed webpack, optimized new features with OOP

---
+++ src/components/popup.js
@@ -0,0 +1,17 @@
+export default class Popup {
+  constructor({ popupSelector }) {
+    this._popupElement = document.querySelector(popupSelector);
+  }
+  open(){
+
+  }
+  close(){
+
+  }
+  _handleEscClose(){
+
+  }
+  setEventListeners(){
+    
+  }
+}
---
+++ src/components/popupWithForms.js
@@ -0,0 +1,5 @@
+import popup from "./popup.js"
+
+class PopupWithForm extends popup{
+    constructor(popupSelector,)
+}
